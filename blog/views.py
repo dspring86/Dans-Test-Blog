48ab56a3b933d130323bf1068ca2849742d271d6
Daniel Spring <dspring@equityeng.com>
2016-01-05 12:45:08 -0500

    Changed and added urls

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(title__contains='Test')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
